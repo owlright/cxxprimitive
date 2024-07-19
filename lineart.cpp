eb72692762af4412b4b085a12642dc40e96b768a
owlright <71282734+owlright@users.noreply.github.com>
2024-07-19 22:09:32 +0800

    测试多核心

--- lineart.cpp
+++ lineart.cpp
@@ -12,7 +12,7 @@
 #include "timer.h"
 namespace fs = std::filesystem;
 using namespace primitive;
-const int try_number = 10000;
+const int try_number = 1000;
 std::pair<int, int> PickTwoUniqueNumbers()
 {
     int first = rand() % 4; // 生成1到4之间的随机数作为第一个数字
@@ -23,19 +23,49 @@ std::pair<int, int> PickTwoUniqueNumbers()
     return std::make_pair(first, second); // 返回一对不重复的数字
 }
 
-double Energy(const RasterizedLines* lines, const unsigned char* grayImage, int width, int height)
+std::pair<double, unsigned char> Energy(const RasterizedLines& lines, const unsigned char* grayImage, int width, int height)
 {
     double energy = 0;
-
-    for (int i = 0; i < lines->h; i++) {
-        auto line = lines->lines[i];
+    // 计算平均值
+    double avg = 0.0;
+    int pixelCount = 0;
+    for (int i = 0; i < lines.h; i++) {
+        auto line = lines.lines[i];
         for (int x = line.left; x <= line.right; x++) {
-            energy += pow(grayImage[line.y * width + x] - lines->r, 2);
+            avg += grayImage[line.y * width + x];
+            pixelCount++;
         }
     }
-    return sqrt(energy);
+    if (pixelCount == 0) {
+        return std::make_pair(INFINITY, 255);
+    }
+    avg /= pixelCount;
+    for (int i = 0; i < lines.h; i++) {
+        auto line = lines.lines[i];
+        for (int x = line.left; x <= line.right; x++) {
+            energy += pow(grayImage[line.y * width + x] - avg, 2);
+        }
+    }
+    return std::make_pair(sqrt(energy), avg);
 }
 
+void FixEdge(int&x, int& y, const int width, const int height, int edgeId)
+{
+     switch (edgeId) {
+        case 0:
+           y = 0;
+            break;
+        case 1:
+            x = width - 1;
+            break;
+        case 2:
+            y = height - 1;
+            break;
+        case 3:
+            x = 0;
+            break;
+        }
+}
 std::vector<int> GeneratePositions(int width, int height)
 {
     auto [first, second] = PickTwoUniqueNumbers();
@@ -49,56 +79,33 @@ std::vector<int> GeneratePositions(int width, int height)
                 positions[i] = rand() % height;
             }
         }
-        switch (first) {
-        case 1:
-            positions[1] = 0;
-            break;
-        case 2:
-            positions[0] = width - 1;
-            break;
-        case 3:
-            positions[1] = height - 1;
-            break;
-        case 4:
-            positions[0] = 0;
-            break;
-        }
-        switch (second) {
-        case 1:
-            positions[3] = 0;
-            break;
-        case 2:
-            positions[2] = width - 1;
-            break;
-        case 3:
-            positions[3] = height - 1;
-            break;
-        case 4:
-            positions[2] = 0;
-            break;
-        }
+        FixEdge(positions[0], positions[1], width, height, first);
+        FixEdge(positions[2], positions[3], width, height, second);
     }
 
     return positions;
 }
 
-double MinEnergy(const RasterizedLines* lines, const unsigned char* grayImage, int width, int height, int workerId)
+using OptResult = std::pair<double, Line>;
+OptResult MinEnergy(const unsigned char* grayImage, const int width, const int height, int workerId)
 {
-    double min_energy = INFINITY;
+    double minEnergy = INFINITY;
+    Line minLine;
+    unsigned char avgBlack;
     for (auto i = 0; i < try_number; i++) {
         srand(workerId * try_number + i);
         auto positions = GeneratePositions(width, height);
         Line l(positions);
         RasterizedLines lines = l.Rasterize();
-        lines.r = rand() % 256;
-        lines.g = rand() % 256;
-        lines.b = rand() % 256;
-        auto result = Energy(&lines, grayImage, width, height);
-        if (result < min_energy) {
-            min_energy = result;
+        auto [egy, color] = Energy(lines, grayImage, width, height);
+        l.r = l.g = l.b = color;
+        if (egy < minEnergy) {
+            minEnergy = egy;
+            minLine = l;
         }
     }
-    return min_energy;
+
+    return std::make_pair(minEnergy, minLine);
 }
 
 int main()
@@ -108,49 +115,49 @@ int main()
     std::string pathStr = p.string();
     auto pos = pathStr.find("primitive");
     auto project_path = fs::path(pathStr.substr(0, pos + std::string("primitive").length()));
-    auto imagePath = project_path.append("images").append("starrynight.jpg");
+    auto imagePath = project_path.append("images").append("lenna.png");
     int width, height, nChannels;
     // stbi_set_flip_vertically_on_load(1);
     unsigned char* imageData = stbi_load(imagePath.string().c_str(), &width, &height, &nChannels, 0);
 
     printf("width: %d, height: %d, nrComponents: %d\n", width, height, nChannels);
     auto grayImage = rgb2grayMerged(imageData, width, height);
-    Line l(145, 0, 0, 34);
-    RasterizedLines lines = l.Rasterize();
-    // Display(&lines);
+
     ThreadPool pool(6);
     // enqueue and store future
     {
-        std::cout << "Using ThreadPool" << std::endl;
         Timer _;
-        std::vector<std::future<double>> results;
+        std::vector<std::future<OptResult>> results;
+        // std::vector<OptResult> results;
         for (auto i = 0; i < 6; i++) {
-            auto result = pool.enqueue(MinEnergy, &lines, grayImage, width, height, i);
+            auto result = pool.enqueue(MinEnergy, grayImage, width, height, i);
+            // auto result = MinEnergy(grayImage, width, height, i);
             results.push_back(std::move(result));
         }
         for (auto& result : results) {
-            auto energy = result.get();
-            std::cout << "energy: " << energy << std::endl;
+            auto r = result.get();
+            // auto r = result;
+            std::cout << "energy: " << r.first << " " << r.second << std::endl;
         }
     }
-    {
-        std::cout << "Not using ThreadPool" << std::endl;
-        Timer _;
-        for (auto i = 0; i < 6 * try_number; i++) {
-            srand(i);
+    // {
+    //     std::cout << "Not using ThreadPool" << std::endl;
+    //     Timer _;
+    //     for (auto i = 0; i < 6 * try_number; i++) {
+    //         srand(i);
 
-            auto positions = GeneratePositions(width, height);
+    //         auto positions = GeneratePositions(width, height);
 
-            Line l(positions);
-            RasterizedLines lines = l.Rasterize();
-            lines.r = rand() % 256;
-            lines.g = rand() % 256;
-            lines.b = rand() % 256;
-            auto result = pool.enqueue(Energy, &lines, grayImage, width, height);
-            auto energy = result.get();
-        }
-    }
-    DestroyRasterizedLines(&lines);
+    //         Line l(positions);
+    //         RasterizedLines lines = l.Rasterize();
+    //         lines.r = rand() % 256;
+    //         lines.g = rand() % 256;
+    //         lines.b = rand() % 256;
+    //         auto result = pool.enqueue(Energy, &lines, grayImage, width, height);
+    //         auto energy = result.get();
+    //     }
+    // }
+
     free(imageData);
     free(grayImage);
 }
