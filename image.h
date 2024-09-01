3647bc067bbc39a1754b052212120f6487e35c85
owlright <71282734+owlright@users.noreply.github.com>
2024-09-01 19:14:51 +0800

    refactor: Add alpha channel to Color struct

--- image.h
+++ image.h
@@ -3,21 +3,31 @@
 #include <ostream>
 namespace primitive {
 struct Color {
-    unsigned char r, g, b;
+    unsigned char r, g, b, a;
     Color()
         : r(0)
         , g(0)
         , b(0)
+        , a(255)
     {
     }
     Color(unsigned char r, unsigned char g, unsigned char b)
         : r(r)
         , g(g)
         , b(b)
+        , a(255)
     {
     }
-    friend std::ostream& operator<<(std::ostream& os, const Color& color) {
-        os << "Color(" << (int)color.r << ", " << (int)color.g << ", " << (int)color.b << ")";
+    Color(unsigned char r, unsigned char g, unsigned char b, unsigned char a)
+        : r(r)
+        , g(g)
+        , b(b)
+        , a(a)
+    {
+    }
+    friend std::ostream& operator<<(std::ostream& os, const Color& color)
+    {
+        os << "Color(" << (int)color.r << ", " << (int)color.g << ", " << (int)color.b << ", " << (int)color.a << ")";
         return os;
     };
 };
